1f182c91ecf03f19431bdba5bf61d3502786399d
Pranil Rathod <rathod1337@gmail.com>
2019-12-15 15:02:13 +0530

    fix server.js

--- server.js
+++ server.js
@@ -18,25 +18,6 @@ app.get('/mockData',jsonParser,(req,res)=>{
     res.send(mockData)
 })
 
-app.get('/alpha_sorted_md',jsonParser,(req,res)=>{
-    var tempDataForSortAlpha = mockData
-    res.header("Content-Type",'application/json')
-      function compare(a,b){
-        if (a.name < b.name)
-          return -1;
-        if (a.name > b.name)
-          return 1;
-        return 0;
-      }
-    res.send(tempDataForSortAlpha.sort(compare))
-})
-
-app.get('/date_sorted_md',jsonParser,(req,res)=>{
-    res.header("Content-Type",'application/json')
-    var tempDataForSortDate = mockData
-    res.send(tempDataForSortDate.sort((a, b) => Date.parse(a.dateLastEdited) - Date.parse(b.dateLastEdited)))
-})
-
 app.listen(port,()=>{
     console.log(`Server Listening on Port ${port}`)
 })
